b3ced4d07dafebf915f778d72147f29423b26421
Marek Ott <marekott94@gmail.com>
2019-01-17 19:25:02 +0100

    Add solution with UnitTests. Two simple tests has been written

---
+++ IntelligentConsoleSnake/IntelligentConsoleSnakeTests1/NN/NeuralNetworkTests.cs
@@ -0,0 +1,18 @@
+﻿using IntelligentConsoleSnake.NN;
+using Xunit;
+
+namespace IntelligentConsoleSnakeTests1.NN
+{
+	public class NeuralNetworkTests
+	{
+		[Fact()]
+		public void NeuralNetworkConstructorCheckIfBadWeightsInArrayInSetWeightsIsNotThrown()
+		{
+			int numberOfInputs = 5;
+			int numberOfNeuronsInFirstHiddenLayer = 5;
+			int numberOfOutputNeurons = 2;
+
+			var neuralNetwork = new NeuralNetwork(numberOfInputs, numberOfNeuronsInFirstHiddenLayer, numberOfOutputNeurons);
+		}
+	}
+}
