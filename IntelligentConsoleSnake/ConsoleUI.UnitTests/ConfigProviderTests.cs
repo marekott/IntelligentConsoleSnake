a12eab14056f2f7f4423f8c08ec4c347f4301c51
Marek Ott <marekott94@gmail.com>
2020-07-08 19:27:14 +0200

    Added unit tests project

---
+++ IntelligentConsoleSnake/ConsoleUI.UnitTests/ConfigProviderTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleUI.UnitTests
+{
+    [TestFixture]
+    public class ConfigProviderTests
+    {
+        [Test]
+        public void GetMapHeightTest()
+        {
+            // arrange
+            const int expected = 20;
+            var configProvider = new ConfigProvider();
+
+            // act
+            var actual = configProvider.GetMapHeight();
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetMapWidthTest()
+        {
+            // arrange
+            const int expected = 50;
+            var configProvider = new ConfigProvider();
+
+            // act
+            var actual = configProvider.GetMapWidth();
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetGameLeftOffsetTest()
+        {
+            // arrange
+            const int expected = 10;
+            var configProvider = new ConfigProvider();
+
+            // act
+            var actual = configProvider.GetGameLeftOffset();
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetGameTopOffsetTest()
+        {
+            // arrange
+            const int expected = 4;
+            var configProvider = new ConfigProvider();
+
+            // act
+            var actual = configProvider.GetGameTopOffset();
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ IntelligentConsoleSnake/ConsoleUI.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsoleUI.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleUI.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("142b8bb7-8bac-4924-afd2-c47e25d42ee7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
